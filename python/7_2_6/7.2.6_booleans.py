e08fbd99d7c2420ee2da3cddbc169daa6ef8b176
Andres <andrescastrogonzal245@outlook.com>
2026-05-15 02:27:37 -0500

    7.2.6, booleans

---
+++ python/7_2_6/7.2.6_booleans.py
@@ -0,0 +1,14 @@
+print(bool('Hello'))    # True     string with a value
+print(bool('123'))      # True     string with a value
+print(bool(''))         # False    string with empty value
+print(bool(123))        # True     integer with a value
+print(bool(123.45))     # True     float with a value
+print(bool(2))          # True     integer with a value
+print(bool(1))          # True     integer with a value
+print(bool(0))          # False    integer with zero value
+print(bool('0'))        # True     string with a value
+print(bool(-1))         # True     integer with a value
+print(bool('True'))     # True     string with a value
+print(bool('False'))    # True     string with a value
+print(bool(True))       # True     boolean with a True value
+print(bool(False))      # False    boolean with a False value
