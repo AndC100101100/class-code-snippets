9e3999665fcfcf220859fdb81fb14fe4788ac359
Andres <andrescastrogonzal245@outlook.com>
2026-05-15 02:09:30 -0500

    oops

--- python/7_2_4/7.2.4_stringsv1.py
+++ python/7_2_4/7.2.4_stringsv1.py
@@ -1,6 +1,8 @@
 phrase = "Hello world"
 print(len(phrase))
 '''
-data types have methods, phrase is a string which has the .count method, a dot notation method that gives us access to the count function so that we can count the number of ocurrences of a character
+data types have methods, phrase is a string which has the .count method, 
+a dot notation method that gives us access to the count function so that 
+we can count the number of ocurrences of a character
 '''
 print(phrase.count("l"))
