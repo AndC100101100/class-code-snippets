40cfd03c37ce0cd5cdc5d384ad9702d1ccd567b7
Andres <andrescastrogonzal245@outlook.com>
2026-05-15 00:44:20 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# About this repo
+this is a repo to save code snippets written during the coding module for SANS GFACT, handwritten
---
+++ python/7.2.3_variables.py
@@ -0,0 +1,15 @@
+my_string = "I can write whatever I like here"
+my_int = 1337
+my_float = 42.1234
+my_bool = False
+
+print(my_string)
+print(my_int)
+print(my_float)
+print(my_bool)
+
+print(type(my_string))
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+
