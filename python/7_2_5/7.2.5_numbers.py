b7c0e067d0623b375a7e8de027c4ba17ebeaa16f
Andres <andrescastrogonzal245@outlook.com>
2026-05-15 02:05:47 -0500

    reformat, 7.2.5 numbers

---
+++ python/7_2_5/7.2.5_numbers.py
@@ -0,0 +1,15 @@
+print(8888 * 77)
+
+print(999 / 2)
+
+print(1 + 2 * 3) # not 9
+
+print((1 + 2) * 3) # this is 9
+
+print(0.2 + 0.4) # floating point math, not rounded
+
+print(round(0.2 + 0.4, 1)) # rounded to the first decimal
+
+print("1" * 77) # will print 1 77 times
+
+print(1 * 77) # will print 77
