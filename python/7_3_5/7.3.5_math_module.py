3fa7eb2b20517a4945840daf3616c9fda82aef72
Andres <andrescastrogonzal245@outlook.com>
2026-05-19 16:42:38 -0500

    7.3.5 Math

---
+++ python/7_3_5/7.3.5_math_module.py
@@ -0,0 +1,12 @@
+import math
+
+print(math.sqrt(81))             # 9.0
+print(math.ceil(7.43))           # 8
+print(math.floor(6.17))          # 6
+print(math.pi)                   # 3.141592653589793
+print(math.cos(43.21))           # 0.7163096496046328
+print(math.sin(43.21))           # -0.6977825491392631
+print(math.tan(43.21))           # -0.9741353470868418
+print(math.isnan(987))           # False
+print(math.isnan(float('nan')))  # True
+print(math.degrees(12.34))       # 707.0299191914359
