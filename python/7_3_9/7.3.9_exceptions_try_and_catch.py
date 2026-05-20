3168a54d3153da13f24dcbc091377e3c3830a296
Andres <andrescastrogonzal245@outlook.com>
2026-05-20 18:11:19 -0500

    7.3.8 Exceptions

---
+++ python/7_3_9/7.3.9_exceptions_try_and_catch.py
@@ -0,0 +1,21 @@
+print('Enter 2 numbers and I\'ll divide them')
+print('Enter "q" to quit\n')
+
+while True:
+    first_input = input("First Number: ")
+    if first_input == "q":
+        break
+
+    second_input = input("Second Number: ")
+    if second_input == "q":
+        break
+
+    try:
+        answer = int(first_input) / int(second_input)
+    except ZeroDivisionError:
+        print('Can\'t divide by zero, try again')
+    else:
+        print('Then answer is: ' + str(answer))
+        print('Try Again')
+
+print('Ciao')
