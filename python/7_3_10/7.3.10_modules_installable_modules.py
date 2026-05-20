31f1446fe4989fddb83b3489c5206a1972fd597e
Andres <andrescastrogonzal245@outlook.com>
2026-05-20 18:39:03 -0500

    7.3.10, modules, installable modules

---
+++ python/7_3_10/7.3.10_modules_installable_modules.py
@@ -0,0 +1,11 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+data = b'secrte_message'
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX) 
+ciphertext, tag = cipher.encrypt_and_digest(data)
+
+print(ciphertext)
+print(tag)
